638df44d35ed0fe441490ddf8d2f193691fb450e
Chaeun <codns0621@gmail.com>
2025-02-03 18:39:45 +0900

    MyBlog MongoDB연결 구현

---
+++ myNode/myBlog/app.js
@@ -0,0 +1,25 @@
+require("dotenv").config(); //.env에 있는 변수 가져오기
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
+
+const app = express();
+const port = process.env.PORT || 3000; //.env에 PORT가 없으면 3000번 포트 사용
+
+//DB연결
+connectDb();
+
+//레이아웃과 뷰 엔진 설정
+app.use(expressLayouts);
+app.set("view engine","ejs");
+app.set("views","./views");
+
+//정적 파일
+app.use(express.static("public"));
+
+// 루트(/)경로로 접속하면 routes\main.js의 라우트 사용
+app.use("/",require("./routes/main"));
+
+app.listen(port,() => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ myNode/myBlog/routes/main.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asynchandler = require("express-async-handler");
+
+router.get(
+    ["/","/home"],
+asynchandler(async (req, res) => {
+    const locals = {
+        title: "Home",
+    };
+
+    const data = await Post.find({}); //데이터베이스에 있는 데이터 모두 가져오기
+    res.render("index",{locals,data,layout: mainLayout});
+  })
+);
+
+router.get("/about",(req,res) => {
+    //about.ejs를 렌더링하는데 mainLayout 레이아웃으로 감싸기
+    res.render("about",{layout:mainLayout});
+});
+
+module.exports = router;
+
+
